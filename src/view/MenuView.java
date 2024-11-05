cae02a005807359e167b8b12dc27a6720da41afc
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-11-05 11:47:42 -0300

    :sparkles: feat: Enhance MenuView with ANSI color formatting for improved user interface

--- src/view/MenuView.java
+++ src/view/MenuView.java
@@ -1,22 +1,25 @@
 package view;
 
 public class MenuView {
+    
+
     public void menuPrincipal() {
+        System.out.println("");
         System.out.println("*****************************************************");
         System.out.println("                                                     ");
-        System.out.println("                Santo André Bank                     ");
+        System.out.println( Cores.TEXT_CYAN_BOLD_BRIGHT + "                Santo André Bank                     "+ Cores.TEXT_RESET);
         System.out.println("                                                     ");
         System.out.println("*****************************************************");
         System.out.println("                                                     ");
-        System.out.println("            1 - Criar Conta                          ");
+        System.out.println(Cores.TEXT_GREEN_BRIGHT +"            1 - Criar Conta                          ");
         System.out.println("            2 - Listar todas as Contas               ");
         System.out.println("            3 - Buscar Conta por Numero              ");
         System.out.println("            4 - Atualizar Dados da Conta             ");
         System.out.println("            5 - Apagar Conta                         ");
         System.out.println("            6 - Sacar                                ");
         System.out.println("            7 - Depositar                            ");
-        System.out.println("            8 - Transferir valores entre Contas      ");
-        System.out.println("            9 - Sair                                 ");
+        System.out.println("            8 - Transferir valores entre Contas      "+ Cores.TEXT_RESET);
+        System.out.println(Cores.TEXT_RED_BRIGHT + "            9 - Sair                                 "+ Cores.TEXT_RESET);
         System.out.println("                                                     ");
         System.out.println("*****************************************************");
         System.out.println("Entre com a opção desejada:                          ");
@@ -28,7 +31,7 @@ public class MenuView {
         System.out.println("*****************************************************");
         System.out.println("                                                     ");
         System.out.println("            Desenvolvido por:                        ");
-        System.out.println("            - Rodrigo Santos( Rudr1gu )              ");
+        System.out.println(Cores.TEXT_GREEN_BRIGHT + "            - Rodrigo Santos( Rudr1gu )              " + Cores.TEXT_RESET);
         System.out.println("            - github.com/Rudr1gu                     ");
         System.out.println("                                                     ");
         System.out.println("*****************************************************");
