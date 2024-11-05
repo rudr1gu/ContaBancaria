28a45e4a6a2d33408c307e8c6ca3e47e8f0bcc7a
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-11-05 10:59:24 -0300

    :sparkles: feat: Add ContaCorrente class

---
+++ src/model/ContaCorrente.java
@@ -0,0 +1,19 @@
+package model;
+
+public class ContaCorrente extends Conta{
+    private float limite;
+
+    public ContaCorrente(int numeroConta, int agencia, int tipo, String nomeTitular, float saldo, float limite) {
+        super(numeroConta, agencia, tipo, nomeTitular, saldo);
+        this.limite = limite;
+    }
+
+    public float getLimite() {
+        return limite;
+    }
+
+    public void setLimite(float limite) {
+        this.limite = limite;
+    }
+
+}
