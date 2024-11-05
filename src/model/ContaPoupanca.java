935cd8870e7be16dff74b6a524364adbaaa5282a
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-11-05 10:57:48 -0300

    :sparkles: feat: Add ContaPoupaca

---
+++ src/model/ContaPoupanca.java
@@ -0,0 +1,18 @@
+package model;
+
+public class ContaPoupanca extends Conta{
+    private int diaAniversario;
+
+    public ContaPoupanca(int numeroConta, int agencia, int tipo, String nomeTitular, float saldo, int diaAniversario) {
+        super(numeroConta, agencia, tipo, nomeTitular, saldo);
+        this.diaAniversario = diaAniversario;
+    }
+
+    public int getDiaAniversario() {
+        return diaAniversario;
+    }
+
+    public void setDiaAniversario(int diaAniversario) {
+        this.diaAniversario = diaAniversario;
+    }
+}
