224488f64d77fd60f4fdeb7edb34e551a39591c0
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-11-05 11:14:59 -0300

    :sparkles: feat: Replace Main class with Menu class and add MenuView for user interaction

---
+++ src/Menu.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+import view.MenuView;
+
+public class Menu {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        MenuView menuView = new MenuView();
+        int opcao;
+        boolean isRunning = true;
+
+        while (isRunning) {
+            menuView.menuPrincipal();
+
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Cadastrar conta");
+                    break;
+                case 2:
+                    System.out.println("Atualizar conta");
+                    break;
+                case 3:
+                    System.out.println("Deletar conta");
+                    break;
+                case 4:
+                    System.out.println("Sacar");
+                    break;
+                case 5:
+                    System.out.println("Depositar");
+                    break;
+                case 6:
+                    System.out.println("Transferir");
+                    break;
+                case 7:
+                    System.out.println("Listar todas as contas");
+                    break;
+                case 8:
+                    System.out.println("Procurar conta por número");
+                    break;
+                case 9:
+                    System.out.println("Saindo...");
+                    isRunning = false;
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+                    break;
+            }
+        }
+        scanner.close();
+    }
+}
