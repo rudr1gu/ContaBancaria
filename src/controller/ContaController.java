bf3ff585863a6d08e8035db3ca34a28cd6c4971d
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-11-08 12:35:31 -0300

    :recycle: Refactor ContaController and ContaRepository method names for clarity

--- src/controller/ContaController.java
+++ src/controller/ContaController.java
@@ -4,52 +4,51 @@ import model.Conta;
 public class ContaController implements ContaRepository {
 
     @Override
-    public void procurarNumero(int numero) {
-        
+    public void procurarPorNumero(int numero) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'procurarPorNumero'");
     }
 
     @Override
     public void listarTodas() {
+        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'listarTodas'");
     }
 
     @Override
     public void cadastrar(Conta conta) {
+        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'cadastrar'");
-        
     }
 
     @Override
     public void atualizar(Conta conta) {
+        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'atualizar'");
     }
 
     @Override
     public void deletar(int numero) {
+        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'deletar'");
     }
 
     @Override
     public void sacar(int numero, float valor) {
+        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'sacar'");
     }
 
     @Override
     public void depositar(int numero, float valor) {
+        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'depositar'");
     }
 
     @Override
     public void transferir(int numeroOrigem, int numeroDestino, float valor) {
+        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'transferir'");
-    }
-
-    public int gerarNumeroConta() {
-        return 0;
-    }
-
-    public Conta buscarConta(int numero) {
-        throw new UnsupportedOperationException("Unimplemented method 'buscarConta'");
     }
 
   
--- src/controller/ContaRepository.java
+++ src/controller/ContaRepository.java
@@ -2,12 +2,15 @@ package controller;
 import model.Conta;
 
 public interface ContaRepository {
-    public void procurarNumero(int numero);
-    public void listarTodas();
-    public void cadastrar(Conta conta);
-    public void atualizar(Conta conta);
-    public void deletar(int numero);
-    public void sacar(int numero, float valor);
-    public void depositar(int numero, float valor);
-    public void transferir(int numeroOrigem, int numeroDestino, float valor);
+    	// CRUD da Conta
+	public void procurarPorNumero(int numero);
+	public void listarTodas();
+	public void cadastrar(Conta conta);
+	public void atualizar(Conta conta);
+	public void deletar(int numero);
+	
+	// Métodos Bancários
+	public void sacar(int numero, float valor);
+	public void depositar(int numero, float valor);
+	public void transferir(int numeroOrigem, int numeroDestino, float valor);
 }
